83a5855b869211ccbb47a8c3dc4bf7524d79be12
ander <andersonline.it@gmail.com>
2022-08-21 02:46:42 -0300

    Exercício 1 - Lista 1

--- java_exercicios_lista1/src/java_exercicios_lista1/_1_MeuPrimeiroPrograma.java
+++ java_exercicios_lista1/src/java_exercicios_lista1/_1_MeuPrimeiroPrograma.java
@@ -1,5 +1,7 @@
 package java_exercicios_lista1;
 
+//1. Escreva um programa que apresente na tela a frase: "Meu primeiro programa!!!"
+
 public class _1_MeuPrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa!!!");
