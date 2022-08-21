db640c05b12f359025865c5634ab61512d724cdc
ander <andersonline.it@gmail.com>
2022-08-21 02:37:29 -0300

    Aulas do curso de Lógica de Programação - ADS

---
+++ aula1_lp/src/aula1_lp/Aula1MinhaPrimeiraClasse.java
@@ -0,0 +1,10 @@
+package aula1_lp;
+
+public class Aula1MinhaPrimeiraClasse {
+	// esse códido é pra avisar que isso aqui não é uma classe, é um programa!
+	public static void main (String[] args) {
+		System.out.println("Hello World !");
+	}
+		
+	 
+}
---
+++ aula2_lp/src/java_ceub_aula2/Aula2VolumeEsfera.java
@@ -0,0 +1,23 @@
+package java_ceub_aula2;
+import java.util.Scanner;
+
+public class Aula2VolumeEsfera {
+	public static void main (String[] args) {
+		//Declaração de variáveis
+		Scanner leitor = new Scanner(System.in);
+		double raio = 0;
+		double volume = 0;
+		
+		//Entrada de dados
+		System.out.print("Digite o raio: ");
+		raio = leitor.nextDouble();
+		leitor.close();
+		
+		// Processamento
+		volume = ((4 * Math.PI * Math.pow(raio, 3)) / 3);
+		
+		//Saída de dados
+		System.out.println ("O volume da esfera é: " + volume);
+	}
+
+}
