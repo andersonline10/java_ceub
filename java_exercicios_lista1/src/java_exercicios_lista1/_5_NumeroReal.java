3700d77990edff20174747221a7454009fcfb1a1
ander <andersonline.it@gmail.com>
2022-08-21 12:42:35 -0300

    Exercício 5 - Lista 1

---
+++ java_exercicios_lista1/src/java_exercicios_lista1/_5_NumeroReal.java
@@ -0,0 +1,24 @@
+// 5. Escreva um programa que solicite ao usuário um número real e ao final apresente na tela o
+// número informado formatado com duas casas decimais da seguinte forma: "Voce informou
+// o numero X.YY"
+
+package java_exercicios_lista1;
+
+import java.util.Scanner;
+
+public class _5_NumeroReal {
+	public static void main (String[] args) {
+		//Declaração de variáveis
+		Scanner leitor = new Scanner(System.in);
+		double numero_real = 0;
+		
+		//Entrada de dados
+		System.out.print("Digite um número real: ");
+		numero_real = leitor.nextDouble();
+		
+		//Saída de dados
+		System.out.printf("O número informado foi o %.2f.", numero_real);
+	}
+	
+
+}
