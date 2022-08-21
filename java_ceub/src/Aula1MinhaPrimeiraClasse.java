193541eaf65c6e1bfbdab015349dc993af402bad
ander <andersonline.it@gmail.com>
2022-08-21 02:29:02 -0300

    Aula 1 - Lógica de Programação

--- java_ceub/src/Aula1MinhaPrimeiraClasse.java
+++
@@ -1,9 +0,0 @@
-
-public class Aula1MinhaPrimeiraClasse {
-	// esse códido é pra avisar que isso aqui não é uma classe, é um programa!
-	public static void main (String[] args) {
-		System.out.println("Hello World !");
-	}
-		
-	 
-}
