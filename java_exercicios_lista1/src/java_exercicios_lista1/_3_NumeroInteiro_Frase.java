c9c72c947bc39198f0e45b98e943b0f4bef585e5
ander <andersonline.it@gmail.com>
2022-08-21 02:53:57 -0300

    Lista 1 - alterações nos exercícios 2 e 3

--- java_exercicios_lista1/src/java_exercicios_lista1/_3_NumeroInteiro_Frase.java
+++ java_exercicios_lista1/src/java_exercicios_lista1/_3_NumeroInteiro_Frase.java
@@ -2,26 +2,22 @@ package java_exercicios_lista1;
 
 import java.util.Scanner;
 
-public class _2_NumeroInteiro {
+public class _3_NumeroInteiro_Frase {
 	public static void main(String[]args) {
-	//	2. Escreva um programa que solicite ao usuário um número inteiro e ao final apresente na tela
-	//	o número informado pelo usuário do programa
-		
+		//3. Escreva um programa que solicite ao usuário um número inteiro e ao final apresente na tela
+		//o número informado da seguinte forma: "Foi informado o valor: X"
+			
 		//Declaração de variáveis
 		Scanner leitor = new Scanner (System.in);
 		int numero = 0;
-		
+			
 		//Entrada de dados
 		System.out.println("Digite um número inteiro: ");
 		numero = leitor.nextInt();
 		
 		leitor.close();
-		
+			
 		//Saída de dados
-		System.out.println(numero);
-		
-		
-		
-	}
-
+		System.out.println("Foi informado o valor: " + numero + ".");	
+		}
 }
